4c85b560a8db10dfc34180c703d5592cd2a610a1
ibipul <islam.bipul@gmail.com>
2025-12-01 23:56:26 +0530

    Setting up the framework

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Neural_Network
-This is a simple repositary to build a Multi Layer Perceptron From Scratch. We will build the Actual Computation Graph from the Nodes up, we will implement backpropagation. We will create the loss function and train a POC MLP on fake data by hand
+# A Neural Network Playground
+ - We will define Nodes of a computation graph 
+ - We will build the Actual Computation Graph on 
+ - We will implement backpropagation on the graph 
+ - We will codify a simple neuron
+ - We will build layers and then finally a MLP
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gradengine",
+    version="0.1.0",
+    author="Bipul Islam",
+    author_email="islam.bipul@gmail.com",
+    description="A tiny scalar-valued autograd engine for computational graphs for backpropagation, and other abstractions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ibipul/gradengine",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+)
